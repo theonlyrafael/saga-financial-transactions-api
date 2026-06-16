ce340480ee226b45f8ec34abd978cc4134104ce8
Rafael Fraga <rafalfraga@gmail.com>
2026-06-16 02:25:43 -0300

    feat: adiciona suporte a conversoes json no rabbitmq

--- conta-service/src/main/java/com/example/conta_service/config/RabbitMQConfig.java
+++ conta-service/src/main/java/com/example/conta_service/config/RabbitMQConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,5 +35,17 @@ public class RabbitMQConfig {
     @Bean
     public Binding ordemCompraBinding(Queue ordemCompraQueue, DirectExchange ordemCompraExchange) {
         return BindingBuilder.bind(ordemCompraQueue).to(ordemCompraExchange).with(ROUTING_KEY_ORDEM_COMPRA);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
     }
 }
