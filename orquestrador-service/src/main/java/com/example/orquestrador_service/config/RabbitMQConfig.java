daef9f6f982a46b3fe0f0b2952853a6504da26a6
Rafael Fraga <rafalfraga@gmail.com>
2026-06-27 01:36:00 -0300

    refactor: classe config de rabbitmq do orquestrador configurada para enviar mensagens para custodia

--- orquestrador-service/src/main/java/com/example/orquestrador_service/config/RabbitMQConfig.java
+++ orquestrador-service/src/main/java/com/example/orquestrador_service/config/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package com.example.orquestrador_service.config;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +9,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-   @Bean
+
+    @Bean
     public MessageConverter jsonMessageConverter() {
         return new JacksonJsonMessageConverter();
     }
-    
+
+    // configura o template para o orquestrador conseguir enviar mensagens para a custódia
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
 }
