8bd9efdbf9bedde0c52b3abffe1da68431d951af
Rafael Fraga <rafalfraga@gmail.com>
2026-06-28 19:18:36 -0300

    style: apaguei uma linha vazia

--- orquestrador-service/src/main/java/com/example/orquestrador_service/repository/TransacaoSagaRepository.java
+++ orquestrador-service/src/main/java/com/example/orquestrador_service/repository/TransacaoSagaRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-// interface que herda os métodos padrão do spring data jpa para salvar e buscar o histórico da saga
 public interface TransacaoSagaRepository extends JpaRepository<TransacaoSaga, UUID> {
 
     // busca a transação específica utilizando o id da ordem original
